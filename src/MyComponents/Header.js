2a096dedaf15346bd7672b5bfc8ab6302b03b2b2
Saurabh.Maddheshiya <er.saurabhmaddheshiya@gmail.com>
2021-05-01 20:45:11 +0530

    fix conflict

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -6,17 +6,17 @@ export default function Header(props) {
     return (
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <Link className="navbar-brand" to={window.location.href}>{props.title}</Link>
+    <Link className="navbar-brand" to="/my-todo-app">{props.title}</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to={window.location.href}>Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/my-todo-app">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/about">About</Link>
+          <Link className="nav-link active" aria-current="page" to="/my-todo-app/about">About</Link>
         </li>
       </ul>
       <form className="d-flex">
